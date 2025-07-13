e86a1370c2d2ee9541820e97079085d74c616b8b
Ivan000095 <ivanlanestosa8@gmail.com>
2025-07-13 10:38:47 -0600

    Update servifinder.sql

--- servifinder.sql
+++ servifinder.sql
@@ -1,185 +1,217 @@
-drop database if exists servifinder;
+DROP DATABASE IF EXISTS servifinder;
 CREATE DATABASE IF NOT EXISTS servifinder;
 USE servifinder;
 
--- roles
-CREATE TABLE roles (
-  Id_Rol int primary key not null auto_increment,
-  valor varchar(20) not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- usuarios
-CREATE TABLE usuarios (
-  Id_Usuario int primary key NOT NULL auto_increment,
-  Id_Rol int not null,
-  Estado enum('activo', 'inactivo') NOT NULL default 'activo',
-  Correo varchar(100) not null,
-  Contraseña varchar(50) NOT NULL,
-  foreign key (Id_Rol) references roles(Id_Rol)
+-- Tabla de usuarios
+CREATE TABLE TUsuarios (
+  Id_Usuario INT PRIMARY KEY AUTO_INCREMENT,
+  Rol ENUM('Administrador', 'Usuario') NOT NULL DEFAULT 'Usuario',
+  Correo VARCHAR(100) NOT NULL UNIQUE,
+  Foto varchar(100) not null,
+  Estado boolean not null DEFAULT TRUE,
+  FechaCreacion date not null,
+  password VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- cliente
-CREATE TABLE clientes (
-  Id_Cliente int primary key NOT NULL auto_increment,
-  Nombre varchar(50) not null,
-  F_Nacimiento date not null,
-  Sexo enum('M', 'F') not null,
-  Latitud decimal(20, 6) not null,
-  Longitud decimal(20, 6) not null,
-  Id_Usuario int not null,
-  foreign KEY (Id_Usuario) references usuarios(Id_Usuario)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- doctores
-CREATE TABLE doctores (
-  Id_Doctor int primary key not null unique auto_increment,
-  Nombre varchar(100) not null,
-  Cedula varchar(50) not null,
-  F_Nacimiento date not null,
-  Lengua varchar(50) not null,
-  Idioma varchar(50) not null,
-  Sexo enum('F','M') not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Doctores y telefonos
-CREATE TABLE doctel (
-  Telefono varchar(20) primary key NOT NULL,
-  Id_Doctor int not null,
-  foreign key (Id_Doctor) references doctores(Id_Doctor)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Especialidad
-CREATE TABLE especialidad (
-  Id_Especialidad int primary key NOT NULL auto_increment,
-  Nombre varchar(100) DEFAULT NULL,
-  Descripcion varchar(200) not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- especialidad de doctores
-CREATE TABLE docesp (
-  Id_DocEsp int primary key not null auto_increment,
-  Id_Doctor int NOT NULL,
-  Id_Especialidad int NOT NULL,
-  Costo decimal(20,6),
-  Horario varchar(50) NOT NULL,
-  Dias_Labo varchar(50) NOT NULL,
-  Latitud decimal(20, 6) not null,
-  Longitud decimal(20, 6) NOT NULL,
-  foreign KEY (Id_doctor) references doctores(Id_Doctor),
-  foreign KEY (Id_Especialidad) references especialidad(Id_Especialidad)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- usuario-doctor
-CREATE TABLE usrdoc (
-  Id_UsuarioDoc int primary key unique NOT NULL auto_increment,
-  Id_Doctor int not null,
-  foreign KEY (Id_Doctor) references doctores(Id_Doctor)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- reseñadoc
-CREATE TABLE reseñadoc (
-  Id_ReseñaDoc int primary key NOT NULL auto_increment,
-  Fecha_hora datetime not null,
-  Puntuacion int(5) not null,
-  Comentario varchar(300),
-  Id_Usuario int not null,
-  Id_Doctor int not null,
-  foreign KEY (Id_Usuario) references usuarios(Id_Usuario),
-  foreign KEY (Id_Doctor) references doctores(Id_Doctor)
+-- Comentarios generales de la página
+CREATE TABLE ComentariosPag (
+  Id_CPagina INT PRIMARY KEY AUTO_INCREMENT,
+  Comentario VARCHAR(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
--- profesionistas
-CREATE TABLE profesionistas (
-  Id_Profesionista int primary key NOT NULL auto_increment,
-  Nombre varchar(100) not null,
-  Lenguas varchar(50) not null,
-  Idiomas varchar(50) not null,
-  Sexo enum('M','F') not null,
-  F_Nacimiento date not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- telefono de profesionistas
-CREATE TABLE telefonoprof (
-  Telefono varchar(20) primary key NOT NULL,
-  Id_profesionista int NOT NULL,
-  foreign KEY (Id_Profesionista) references profesionistas(Id_Profesionista)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- servicio
-CREATE TABLE servicio (
-  Id_servicio int primary key NOT NULL auto_increment,
-  Nombre varchar(100) NOT NULL,
-  Descripcion varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Profesionista y servicio
-CREATE TABLE profserv (
-  Id_ProfServ int primary key NOT NULL auto_increment,
-  Id_Profesionistas int NOT NULL,
-  Id_Servicio int NOT NULL,
-  Costo decimal(20,6),
-  Horario varchar(50) not null,
-  Dias_Lab varchar(50) not null,
-  Latitud decimal(20,6) not null,
-  Longitud decimal(20,6) not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- usuario-profesionista
-CREATE TABLE usrprof (
-  Id_UsuarioProf int primary key NOT NULL unique auto_increment,
-  Id_Profesionista int NOT NULL,
-  Id_Usuario int not null,
-  foreign KEY (Id_Profesionista) references profesionistas(Id_Profesionista),
-  foreign key (Id_Usuario) references usuarios(Id_Usuario)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- reseñaprof
-CREATE TABLE reseñaprof (
-  Id_ReseñaProf int primary key NOT NULL auto_increment,
-  Fecha_hora datetime not null,
-  Puntuacion int(5) not null,
-  Comentario varchar(300),
-  Id_Usuario int not null,
-  Id_profesionista int not null,
-  foreign KEY (Id_Usuario) references usuarios(Id_Usuario),
-  foreign KEY (Id_profesionista) references profesionistas(Id_profesionista)
+-- Clientes
+CREATE TABLE Clientes (
+  Id_Cliente INT PRIMARY KEY AUTO_INCREMENT,
+  CURP VARCHAR(18) UNIQUE,
+  Nombre VARCHAR(50) NOT NULL,
+  F_Nacimiento DATE NOT NULL,
+  Genero ENUM('M', 'F', 'no binario') NOT NULL,
+  Correo VARCHAR(50) NOT NULL UNIQUE,
+  Direccion VARCHAR(200) NOT NULL,
+  Id_Usuario INT NOT NULL,
+  FOREIGN KEY (Id_Usuario) REFERENCES TUsuarios(Id_Usuario)
+    ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- Estableciento
-CREATE TABLE establecimientos (
-  Id_Establecimiento int primary key NOT NULL auto_increment,
-  Nombre varchar(100) DEFAULT NULL,
-  Descripcion varchar(200) DEFAULT NULL,
-  Horario varchar(50) DEFAULT NULL,
-  Dias_labo varchar(50) DEFAULT NULL,
-  Direccion varchar(200) DEFAULT NULL
+-- Comentario en página hecho por cliente
+CREATE TABLE ComentarioPagClien (
+  Id_ComentarioClien INT PRIMARY KEY AUTO_INCREMENT,
+  Id_ComentarioPag INT NOT NULL,
+  Id_Cliente INT NOT NULL,
+  FOREIGN KEY (Id_ComentarioPag) REFERENCES ComentariosPag(Id_CPagina)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Cliente) REFERENCES Clientes(Id_Cliente)
+    ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- reseña establecimientio
-CREATE TABLE reseñaest (
-  Id_ReseñaEst int primary key NOT NULL auto_increment,
-  Comentario varchar(300) DEFAULT NULL,
-  Puntuacion int(5) DEFAULT NULL,
-  Fecha_Hora datetime DEFAULT NULL,
-  Id_Usuario int DEFAULT NULL,
-  Id_Establecimiento int DEFAULT NULL,
-  foreign KEY (Id_Establecimiento) references establecimientos(Id_Establecimiento),
-  foreign KEY (Id_Usuario) references usuarios(Id_Usuario)
+
+-- Doctores
+CREATE TABLE Doctores (
+  Id_Doctor INT PRIMARY KEY AUTO_INCREMENT,
+  Nombre VARCHAR(100) NOT NULL,
+  Cedula VARCHAR(50) NOT NULL,
+  Telefono varchar(20) not null,
+  F_Nacimiento DATE NOT NULL,
+  Idioma VARCHAR(50) NOT NULL,
+  Descripcion VARCHAR(300) NOT NULL,
+  Genero ENUM('M', 'F', 'no binario') NOT NULL NOT NULL,
+  Id_Usuario INT NOT NULL,
+  FOREIGN KEY (Id_Usuario) REFERENCES TUsuarios(Id_Usuario)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Especialidades
+CREATE TABLE Especialidad (
+  Id_Especialidad INT PRIMARY KEY AUTO_INCREMENT,
+  Nombre VARCHAR(100)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Asociación doctor-especialidad
+CREATE TABLE DocEsp (
+  Id_DocEsp INT PRIMARY KEY AUTO_INCREMENT,
+  Id_Doctor INT NOT NULL,
+  Id_Especialidad INT NOT NULL,
+  Costo DECIMAL(6,2),
+  Horario VARCHAR(50),
+  Dias_labo VARCHAR(50),
+  DireccionyRef VARCHAR(300) NOT NULL,
+  FOREIGN KEY (Id_Doctor) REFERENCES Doctores(Id_Doctor)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Especialidad) REFERENCES Especialidad(Id_Especialidad)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Reseñas a doctores
+CREATE TABLE ReseñaDoc (
+  Id_ReseñaDoc INT PRIMARY KEY AUTO_INCREMENT,
+  Fecha_hora DATETIME NOT NULL,
+  Puntuacion INT CHECK (Puntuacion BETWEEN 1 AND 5),
+  Comentario VARCHAR(300),
+  Id_Doctor INT NOT NULL,
+  Id_Usuario INT NOT NULL,
+  FOREIGN KEY (Id_Usuario) REFERENCES TUsuarios(Id_Usuario)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Doctor) REFERENCES Doctores(Id_Doctor)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Comentario página sobre doctores
+CREATE TABLE ComentarioPagDoc (
+  Id_ComentarioDoc INT PRIMARY KEY AUTO_INCREMENT,
+  Id_Doctor INT NOT NULL,
+  Id_ComentarioPag INT NOT NULL,
+  FOREIGN KEY (Id_ComentarioPag) REFERENCES ComentariosPag(Id_CPagina)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Doctor) REFERENCES Doctores(Id_Doctor)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Profesionistas
+CREATE TABLE Profesionistas (
+  Id_Profesionista INT PRIMARY KEY AUTO_INCREMENT,
+  Nombre VARCHAR(100) NOT NULL,
+  Telefono varchar(20) NOT NULL,
+  Idiomas VARCHAR(50) NOT NULL,
+  Genero ENUM('M', 'F', 'no binario') NOT NULL,
+  Descripcion VARCHAR(300) NOT NULL,
+  F_Nacimiento DATE NOT NULL,
+  Id_Usuario INT NOT NULL,
+  FOREIGN KEY (Id_Usuario) REFERENCES TUsuarios(Id_Usuario)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Servicios
+CREATE TABLE Servicios (
+  Id_Servicio INT PRIMARY KEY AUTO_INCREMENT,
+  Nombre VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Asociación profesionista-servicio
+CREATE TABLE ProfServ (
+  Id_ProfServ INT PRIMARY KEY AUTO_INCREMENT,
+  Id_Profesionista INT NOT NULL,
+  Id_Servicio INT NOT NULL,
+  Costo DECIMAL(8, 2),
+  Horario VARCHAR(50),
+  Dias_labo VARCHAR(50),
+  DireccionyRef VARCHAR(300) NOT NULL,
+  FOREIGN KEY (Id_Profesionista) REFERENCES Profesionistas(Id_Profesionista)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Servicio) REFERENCES Servicios(Id_Servicio)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Reseñas a profesionistas
+CREATE TABLE ReseñaProf (
+  Id_ReseñaProf INT PRIMARY KEY AUTO_INCREMENT,
+  Fecha_hora DATETIME NOT NULL,
+  Puntuacion INT CHECK (Puntuacion BETWEEN 1 AND 5),
+  Comentario VARCHAR(300),
+  NombreUsr varchar(50),
+  Id_Profesionista INT NOT NULL,
+  FOREIGN KEY (Id_Profesionista) REFERENCES Profesionistas(Id_Profesionista)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Comentario página sobre profesionistas
+CREATE TABLE ComentarioPagProf (
+  Id_ComentarioProf INT PRIMARY KEY AUTO_INCREMENT,
+  Id_Profesionista INT NOT NULL,
+  Id_ComentarioPag INT NOT NULL,
+  FOREIGN KEY (Id_ComentarioPag) REFERENCES ComentariosPag(Id_CPagina)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Profesionista) REFERENCES Profesionistas(Id_Profesionista)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Establecimientos
+CREATE TABLE Establecimientos (
+  Id_Establecimiento INT PRIMARY KEY AUTO_INCREMENT,
+  Nombre VARCHAR(100),
+  Descripcion VARCHAR(300),
+  Horario VARCHAR(50),
+  Dias_labo VARCHAR(50),
+  Foto varchar(100),
+  DireccionyRef VARCHAR(300)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Reseñas a establecimientos
+CREATE TABLE ReseñaEst (
+  Id_ReseñaEst INT PRIMARY KEY AUTO_INCREMENT,
+  Comentario VARCHAR(300),
+  Puntuacion INT CHECK (Puntuacion BETWEEN 1 AND 5),
+  Fecha_Hora DATETIME,
+  Id_Usuario INT,
+  Id_Establecimiento INT,
+  FOREIGN KEY (Id_Establecimiento) REFERENCES Establecimientos(Id_Establecimiento)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Usuario) REFERENCES TUsuarios(Id_Usuario)
+    ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- cita
-CREATE TABLE cita(
-  Id_Cita int primary key not null unique auto_increment,
-  Id_Cliente int not null,
-  Id_Doctor int not null,
-  Detalle varchar(300) not null,
-  FechaHora_R datetime not null,
-  Fecha_Cita date not null,
-  Estado enum('Pendiente','Aceptada','Rechazada') not null default 'Pendiente',
-  foreign key (Id_Cliente) references clientes(Id_Cliente),
-  foreign key (Id_Doctor) references doctores(Id_Doctor)
+
+-- aquí entran las Citas
+CREATE TABLE Cita (
+  Id_Cita INT PRIMARY KEY AUTO_INCREMENT,
+  Id_Cliente INT NOT NULL,
+  Id_Doctor INT NOT NULL,
+  Detalle VARCHAR(300) NOT NULL,
+  FechaHora_Realizacion DATETIME NOT NULL,
+  FechaHora_Cita DATETIME NOT NULL default 'curdate()',
+  Estado ENUM('Pendiente','Aceptada','Rechazada') NOT NULL DEFAULT 'Pendiente',
+  FOREIGN KEY (Id_Cliente) REFERENCES Clientes(Id_Cliente)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Id_Doctor) REFERENCES Doctores(Id_Doctor)
+    ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
