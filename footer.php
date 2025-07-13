cca9c95352121a743fa8820147befe41c3b3d4cd
Ivan000095 <ivanlanestosa8@gmail.com>
2025-07-13 11:04:57 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,14 +4,11 @@
             <p id="copyright">Todos los derechos reservados por ServiFinder</p>
         </div>
         <div class="col-3">
-            <a href="https://www.instagram.com/servifinder1?igsh=MTZhcHdiYXkwdzlnbA== " class="footer-link"><ion-icon name="logo-instagram"></ion-icon></a>
-            <a href="https://www.facebook.com/share/16S5rNJ3i7/" class="footer-link"><ion-icon name="logo-facebook"></ion-icon></a>
+            <a href="https://www.instagram.com/servifinder1?igsh=MTZhcHdiYXkwdzlnbA==" class="footer-link"><i class="bi bi-facebook text-white fs-2"></i></a>
+            <a href="https://www.facebook.com/share/16S5rNJ3i7/" class="footer-link"><i class="bi bi-instagram text-white fs-2"></i></a>
         </div>
         <div class="col-3">
-            <img id="utselva" src="/ServiFinder Oficial/img/logo-uts.png" alt="UTSelva">
+            <img id="utselva" src="img/logo-uts.png" alt="UTSelva">
         </div>
     </div>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-
 </footer>
