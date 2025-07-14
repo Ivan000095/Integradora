7a187d01a209fd978b9ae787607c53740e006dfb
CesarMon16 <gandhineitor@gmail.com>
2025-07-13 19:26:47 -0600

    Add files via upload

--- modelos/Establecimiento.php
+++ modelos/Establecimiento.php
@@ -1,103 +1,103 @@
-<?php 
+<?php
 
-class Establecimiento 
-{ 
-    public $id_Establecimiento; 
-    public $Nombre; 
-    public $Descripcion; 
-    public $Horario; 
-    public $Dias_lab; 
-    public $Latitud_long; 
-   
-    private static $totalPage;
-    private static $establecimientosPerPage;
-    private static $bd;
+class Establecimiento{
 
-    public function __construct($id_Establecimiento, $Nombre, $Descripcion, $Horario, $Dias_lab, $Latitud_long) 
-    { 
-        $this->id_Establecimiento = $id_Establecimiento; 
-        $this->Nombre = $Nombre; 
-        $this->Descripcion = $Descripcion; 
-        $this->Horario = $Horario; 
-        $this->Dias_lab = $Dias_lab; 
-        $this->Latitud_long = $Latitud_long; 
+    public $Id_Establecimiento;
+    public $Nombre;
+    public $Descripcion;
+    public $Horario;
+    public $Dias_labo;
+    public $Foto;
+    public $DireccionyRef;
+    public $Comentario;
+    public $Puntuacion;
+    public $NombreUsr;
     
-    } 
 
-    public function save($bd){
-        if($stmt = $bd -> prepare ("Insert into establecimiento values (0,?,?,?,?,?)")){
-            $stmt -> bind_param("sssss",
-            $this -> Nombre,
-            $this -> Descripcion,
-            $this -> Horario,
-            $this -> Dias_lab,
-            $this -> Latitud_long
-        );
-        $stmt -> execute();
-        $stmt -> close();
+
+    public static function lista() {
+        $lista = [];
+        include('conexion.php');
+
+        $sql = 'SELECT * FROM establecimientos';
+        $resultado = $con->query($sql);
+        while ($fila = $resultado->fetch_assoc()) {
+            $elemento = new Establecimiento();
+            $elemento->Id_Establecimiento = $fila['Id_Establecimiento'];
+            $elemento->Nombre = $fila['Nombre'];
+            $elemento->Descripcion = $fila['Descripcion'];
+            $elemento->Horario = $fila['Horario'];
+            $elemento->Dias_labo = $fila['Dias_labo'];
+            $elemento->Foto = $fila['Foto'];
+            $elemento->DireccionyRef = $fila['DireccionyRef'];
+           
+           
+            $lista[] = $elemento;
         }
+
+        return $lista;
     }
 
+    public static function find($Id_Establecimiento) {
+        //Aquí buscamos un registro por su id
+        $lista = [];
+        include('conexion.php');
 
-
-    public function genero() 
-    { 
-        return Genero::getGenero($this->idGenero); 
-    } 
- 
-    public static function init($bd, $establecimientosPerPage) 
-    { 
-        self::$bd = $bd; 
-        $stmt = self::$bd->prepare("select count(id_Establecimiento) as cantidad from establecimiento"); 
-        $stmt->execute(); 
-        $stmt->bind_result($cantidad); 
-        $stmt->fetch(); 
-        self::$establecimientosPerPage = $establecimientosPerPage; 
-        self::$totalPage = ceil($cantidad/$establecimientosPerPage); 
+        $sql = 'SELECT * FROM establecimientos WHERE Id_Establecimiento=' . $Id_Establecimiento;
+        $resultado = $con->query($sql);
+        if ($fila = $resultado->fetch_assoc()) {
+            $elemento = new Establecimiento();
+            $elemento->Id_Establecimiento = $fila['Id_Establecimiento'];
+            $elemento->Nombre = $fila['Nombre'];
+            $elemento->Descripcion = $fila['Descripcion'];
+            $elemento->Horario = $fila['Horario'];
+            $elemento->Dias_labo = $fila['Dias_labo'];
+            $elemento->Foto = $fila['Foto'];
+            $elemento->DireccionyRef = $fila['DireccionyRef'];
+            return $elemento;
+        }
+         return NULL;
     }
 
-     public static function totalPage() 
-    { 
-        return self::$totalPage; 
-    }
-
-    public static function getEstablecimientosPage($numPage) 
-{ 
-$Establecimientos = []; 
-$offset = ($numPage-1) * self::$establecimientosPerPage; 
-$stmt = self::$bd->prepare("select * from establecimiento limit ?,?"); 
-$stmt->bind_param("ii",$offset, self::$establecimientosPerPage); 
-$stmt->execute(); 
-$stmt->bind_result($id_Establecimiento, $Nombre, $Descripcion, $Horario, $Dias_lab, 
-$Latitud_long);     
-while($stmt->fetch()) 
-{ 
-array_push($Establecimientos, new Establecimiento($id_Establecimiento, $Nombre, $Descripcion, $Horario, 
-$Dias_lab, $Latitud_long)); 
-} 
-$stmt->close(); 
-return $Establecimientos; 
-} 
-}
-
-
- 
-
-if(isset($argc) && $argc == 2)
-{
-    $mysqli = new mysqli("localhost", "root", "", "servifinde");
-    switch($argv[1])
-    {
-        case "nuevo":
-            $imss = new Establecimiento(0,'IMSS', 'Centro de salud gratuito, en donde te mochan la pierna por una fiebre', '8:00 am - 8:00pm', 'Lunes a viernes', 'Toluca, Chiapas');
-            $imss->save($mysqli);
-            break;
-        case "consulta":
-            Establecimiento::init($mysqli, 2) ;
-            $totalPage = Establecimiento :: totalPage();
-            print("Total de page: $totalPage \n");
-            print_r(Establecimiento::getEstablecimientosPage(1));
-            break;
+    public function save() {
+        //Aquí guardamos datos
+        include('conexion.php');
+        
+        if($this -> Id_Establecimiento==0){
+        $sql = "INSERT INTO establecimientos (Nombre, Descripcion, Horario, Dias_labo, Foto, DireccionyRef) VALUES('". $this -> Nombre . "','". $this -> Descripcion . "', 
+        '". $this -> Horario . "','". $this -> Dias_labo . "','". $this -> Foto . "','". $this -> DireccionyRef . "')";
+        } else {
+            
+            $sql = "UPDATE establecimientos SET Nombre='". $this -> Nombre . "', Descripcion='". $this -> Descripcion . "', Horario='". $this -> Horario . "', Dias_labo='". $this -> Dias_labo . "', Foto='". $this -> Foto . "', DireccionyRef='". $this -> DireccionyRef . "' WHERE Id_Establecimiento=" . $this->Id_Establecimiento;
+            
+        }
+        
+        
+        return $con->query($sql);
     
     }
+
+    public function destroy() {
+        //Aquí eliminamos datos
+        include('conexion.php');
+        $sql = "DELETE FROM establecimientos WHERE Id_Establecimiento=". $this -> Id_Establecimiento;
+        return $con -> query($sql);
+    }
+
+    public function saveres($Id_Establecimiento){
+        include('conexion.php');
+        $sql="INSERT INTO ReseñaEst(Comentario, Puntuacion, NombreUsr, Id_Establecimiento) VALUES('" . $this->Comentario . "', " . $this->Puntuacion . ", '" . $this->NombreUsr . "', $Id_Establecimiento)";
+        return $con->query($sql);
+    }
+
+    public static function findres($Id_Establecimiento) {
+        include('conexion.php');
+        $sql="SELECT * FROM ReseñaEst WHERE Id_Establecimiento=" . $Id_Establecimiento;
+        return $con->query($sql);
+    }
+
+
+
 }
+
+?>
